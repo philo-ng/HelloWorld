a21bc50601403d4401b99441581bf9ac55919f1b
Peter Ng <peter.m.ng@gmail.com>
2015-09-30 22:26:54 -0500

    testing commit

---
+++ HelloWorld/src/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+public class HelloWorld {
+	public static void main(String[] args)
+	{
+		printMessage();
+	}
+
+	private static void printMessage() {
+		System.out.println("Hello, World!");
+	}
+}
